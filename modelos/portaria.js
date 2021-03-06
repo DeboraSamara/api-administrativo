c8fc1a03e754e81337f6de7420962ff17ac9ac7b
DeboraSamara <deborah50play@gmail.com>
2021-03-06 16:58:17 -0300

    commit

---
+++ modelos/portaria.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PortariaSchema = new Schema({
+
+    titulo: {
+        type: String,
+        required: true,
+    }
+
+
+});
+
+module.exports = mongoose.model('Portaria', PortariaSchema);
---
+++ modelos/users.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
+
+
+const UserSchema = new Schema({
+
+    nome: {
+        type: String,
+        required: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+
+    senha: {
+        type: String,
+        required: true
+    },
+
+    cargo: {
+        type: String,
+        required: true,
+    },
+
+    matricula: {
+        type: String,
+    }
+
+});
+
+UserSchema.pre('save', function (next) {
+
+    let user = this;
+
+    bcrypt.hash(user.senha, 10, (err, encrypted) => {
+
+        user.senha = encrypted;
+
+        return next();
+
+    });
+
+});
+
+module.exports = mongoose.model('User', UserSchema);
